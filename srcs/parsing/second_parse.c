30c0bd0e0434f0c4170fbf0233b171e5302a25ff
Gabriela Freitas de albuquerque <gafreita@c2r10s7.42lisboa.com>
2022-08-04 20:39:14 +0100

    starting second parse

---
+++ srcs/parsing/second_parse.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   second_parse.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gafreita <gafreita@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/04 20:05:51 by gafreita          #+#    #+#             */
+/*   Updated: 2022/08/04 20:37:43 by gafreita         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+//expand variables from env
+
+char	*find_env(char	*name)
+{
+	
+}
+
+
+void	expand_var(char *var)
+{
+
+}
