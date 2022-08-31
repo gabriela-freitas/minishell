82147c80ad46b95463adbe1f5faabb32b0694666
Gabriela Freitas de albuquerque <gafreita@c2r10s6.42lisboa.com>
2022-08-31 17:27:02 +0100

    error on env with multiple args

---
+++ srcs/builtins/env.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gafreita <gafreita@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/31 17:09:35 by gafreita          #+#    #+#             */
+/*   Updated: 2022/08/31 17:25:23 by gafreita         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/*prints env variables*/
+static void	print_env(void)
+{
+	t_env	*aux;
+
+	aux = (base()->env);
+	while (aux)
+	{
+		if (aux->content)
+			printf("%s=%s\n", aux->name, aux->content);
+		else
+			printf("%s=\n", aux->name);
+		aux = aux->next;
+	}
+}
+
+void	env(char **args)
+{
+	int	i;
+
+	i = -1;
+	while (args[++i])
+		;
+	if (i == 1)
+		print_env();
+	else
+	{
+		error_message("env: ‘", args[1], "’: No such file or directory");
+		base()->errnumb = 127;
+	}
+}
